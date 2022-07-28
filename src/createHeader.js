c37fb02c91c8ba1ea7fd1081b0e05ef61376c4f7
Japmeet Brar <Japmeetb@hotmail.com>
2022-07-28 14:47:50 -0400

    added HTML webpack plugin

--- src/createHeader.js
+++ src/createHeader.js
@@ -1,69 +1,66 @@
-import OdinLogo from './imgs/odin-logo.svg';
+import OdinLogo from "./imgs/odin-logo.svg";
 
-export default function createHeader(){
-    let content = document.querySelector('#content');
+export default function createHeader() {
+  let header = document.createElement("div");
+  header.className = "header";
 
+  let title = document.createElement("h1");
+  title.textContent = "JAPS WING SHOP";
+  title.className = "title";
 
-    let header = document.createElement('div');
-    header.className = 'header';
+  let navBar = document.createElement("div");
+  navBar.className = "nav-bar";
 
-    let title = document.createElement('h1');
-    title.textContent = "JAPS WING SHOP";
-    title.className = 'title';
+  let mainPageBtn = document.createElement("button");
+  mainPageBtn.className = "main-page";
+  mainPageBtn.textContent = "Main";
 
-    let navBar = document.createElement('div');
-    navBar.className = 'nav-bar';
+  let contactPageBtn = document.createElement("button");
+  contactPageBtn.className = "contact-page";
+  contactPageBtn.textContent = "Contact";
 
-    let mainPageBtn = document.createElement('button');
-    mainPageBtn.className = 'main-page';
-    mainPageBtn.textContent = 'Main';
+  let menuPageBtn = document.createElement("button");
+  menuPageBtn.className = "menu-page";
+  menuPageBtn.textContent = "Menu";
 
-    let contactPageBtn = document.createElement('button');
-    contactPageBtn.className = 'contact-page';
-    contactPageBtn.textContent = 'Contact';
+  navBar.append(mainPageBtn, contactPageBtn, menuPageBtn);
 
-    let menuPageBtn = document.createElement('button');
-    menuPageBtn.className = 'menu-page';
-    menuPageBtn.textContent = 'Menu';
+  header.append(title, navBar);
 
-    navBar.append(mainPageBtn, contactPageBtn, menuPageBtn);
+  let footer = document.createElement("div");
+  footer.className = "footer";
 
-    header.append(title, navBar);
-    
-    let footer = document.createElement('div');
-    footer.className = 'footer';
+  let odinRefContainer = document.createElement("div");
+  odinRefContainer.className = "footer-text";
 
-    let odinRefContainer = document.createElement('div');
-    odinRefContainer.className = 'footer-text';
+  let odinRefText = document.createElement("p");
+  odinRefText.textContent = "Created as a part of TheOdinProject Curriculum";
 
-    let odinRefText = document.createElement('p')
-    odinRefText.textContent = 'Created as a part of TheOdinProject Curriculum'
-    
-    let odinRefLink = document.createElement('a');
-    odinRefLink.href = 'https://www.theodinproject.com/';
+  let odinRefLink = document.createElement("a");
+  odinRefLink.href = "https://www.theodinproject.com/";
 
-    let odinLogo = new Image();
-    odinLogo.className = 'odin-icon';
-    odinLogo.src = OdinLogo;
+  let odinLogo = new Image();
+  odinLogo.className = "odin-icon";
+  odinLogo.src = OdinLogo;
 
-    odinRefLink.append(odinLogo)
+  odinRefLink.append(odinLogo);
 
-    odinRefContainer.append(odinRefText, odinRefLink);
+  odinRefContainer.append(odinRefText, odinRefLink);
 
-    let githubRefContainer = document.createElement('div');
-    githubRefContainer.className = 'footer-text';
+  let githubRefContainer = document.createElement("div");
+  githubRefContainer.className = "footer-text";
 
-    let githubRefText = document.createElement('p');
-    githubRefText.textContent = 'Copyright © 2021 JapmeetBrar';
+  let githubRefText = document.createElement("p");
+  githubRefText.textContent = "Copyright © 2021 JapmeetBrar";
 
-    let githubRefLink = document.createElement('a');
-    githubRefLink.href = 'https://github.com/JapmeetBrar/';
-    githubRefLink.className = 'fa fa-github';
+  let githubRefLink = document.createElement("a");
+  githubRefLink.href = "https://github.com/JapmeetBrar/";
+  githubRefLink.className = "fa fa-github";
 
-    githubRefContainer.append(githubRefText, githubRefLink);
+  githubRefContainer.append(githubRefText, githubRefLink);
 
-    footer.append(odinRefContainer, githubRefContainer);
+  footer.append(odinRefContainer, githubRefContainer);
 
-    content.append(header);
-    content.append(footer);
+  content.append(header);
+  content.append(footer);
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
-import './style.css';
-import createHeader from './createHeader';
-import loadMainPage from './mainPageLoad';
-import loadContactPage from './contactPageLoad';
-import loadMenuPage from './menuPageLoad';
+import "./style.css";
+import createHeader from "./createHeader";
+import loadMainPage from "./mainPageLoad";
+import loadContactPage from "./contactPageLoad";
+import loadMenuPage from "./menuPageLoad";
 
-const LoadWebpage = (function(){
-    createHeader();
+const LoadWebpage = (function () {
+  let content = document.createElement("div");
+  content.id = "content";
+  document.body.appendChild(content);
+
+  let iconLink = document.createElement("link");
+  iconLink.rel = "stylesheet";
+  iconLink.href =
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
+
+  document.head.appendChild(iconLink);
+
+  createHeader();
+  loadMainPage();
+
+  let mainPageBtn = document.querySelector(".main-page");
+  let contactPageBtn = document.querySelector(".contact-page");
+  let menuPageBtn = document.querySelector(".menu-page");
+
+  mainPageBtn.addEventListener("click", function () {
+    content.removeChild(document.querySelector(".card"));
     loadMainPage();
-    let content = document.querySelector('#content');
-    let mainPageBtn = document.querySelector('.main-page');
-    let contactPageBtn = document.querySelector('.contact-page');
-    let menuPageBtn = document.querySelector('.menu-page');
-
-    mainPageBtn.addEventListener('click', function(){
-        content.removeChild(document.querySelector('.card'));
-        loadMainPage();
-    })
-    contactPageBtn.addEventListener('click', function(){
-        content.removeChild(document.querySelector('.card'));
-        loadContactPage();
-    })
-    menuPageBtn.addEventListener('click', function(){
-        content.removeChild(document.querySelector('.card'));
-        loadMenuPage();
-    })
-})()
+  });
+  contactPageBtn.addEventListener("click", function () {
+    content.removeChild(document.querySelector(".card"));
+    loadContactPage();
+  });
+  menuPageBtn.addEventListener("click", function () {
+    content.removeChild(document.querySelector(".card"));
+    loadMenuPage();
+  });
+})();
